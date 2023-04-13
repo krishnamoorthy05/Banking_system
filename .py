90409ef9071ac0cadb11a642121979664fd34a64
krishnamoorthy s <129971645+krishnamoorthy05@users.noreply.github.com>
2023-04-13 21:04:16 +0530

    Create .py

---
+++ .py
@@ -0,0 +1,52 @@
+class bank:
+    bank_name = "nation bank"
+    branch ="chennai"
+    def __init__(self,c_name,acc_no,balance):
+        self.c_name=c_name
+        self.acc_no=acc_no
+        self.balance=balance
+    def main(self):
+        print()
+        msg=int(input(' Enter 1 to display account details : \n Enter 2 to Check Balance : \n Enter 3 to Deposit  : \n Enter 4 to withdraw : \n Enter 5 to Quit : '))
+        if msg==1:
+            self.display()
+            self.main()
+        elif msg==2:
+            self.balances()
+            self.main()
+        elif msg==3:
+            self.deposit()
+            self.main()
+        elif msg==4:
+            self.withdraw()
+            self.main()
+        elif msg==5:
+            print("Thank for Banking with us!!")
+            return
+        else:
+            print("invalid input")
+            self.main()
+    def display(self):
+        print(f'BankName :{bank.bank_name}',f'Branck :{bank.branch}',f'Name : {self.c_name}',f'Account number : {self.acc_no}',sep="\n")
+    def balances(self):
+        print(f'Account Balance : {self.balance}')
+    def deposit(self):
+        amount=int(input("Enter the amount to be deposited :Rs "))
+        self.balance=self.add(self.balance,amount)
+        print(f'Rs {amount} added successfull current balance :Rs {self.balance}')
+    def withdraw(self):
+        amount=int(input("Enter the amount to withdraw :Rs "))
+        if self.balance >= amount :
+            self.balance=self.sub(self.balance,amount)
+            print(f'Rs {amount} withdraw successfull current balance :Rs {self.balance}')
+        else:
+            print(f'Your Balance is {self.balance} : insufficient balance to withdraw {amount}')
+    @staticmethod
+    def add(a,b):
+        return a+b
+    @staticmethod
+    def sub(a,b):
+        return a-b
+c1=bank('krishna',20005486123,1000)
+c1.main()
+        
